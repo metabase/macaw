16ce451d195534fb172cb29dffc337b997d62102
Tim Macdonald <tim@tsmacdonald.com>
2024-02-13 14:24:49 +0000

    WIP

--- src/macaw/core.clj
+++ src/macaw/core.clj
@@ -1,13 +1,20 @@
 (ns macaw.core
   (:import
+   (net.sf.jsqlparser
+    Model)
    (net.sf.jsqlparser.parser
     CCJSqlParserUtil)
+   (net.sf.jsqlparser.schema
+    Column)
    (net.sf.jsqlparser.statement
-    Statement)
+    Statement
+    StatementVisitor)
+   (net.sf.jsqlparser.statement.update
+    Update)
    (net.sf.jsqlparser.util
     TablesNamesFinder)))
 
-(set! *warn-on-reflection* true)
+#_(set! *warn-on-reflection* true)
 
 (defn query->tables
   "Given a parsed query (i.e., a subclass of `Statement`) return a list of fully-qualified table names found within it.
@@ -17,15 +24,52 @@
   (let [table-finder (TablesNamesFinder.)]
     (.getTableList table-finder parsed-query)))
 
+(defprotocol ConjColumn
+  (conj-column! [x known-columns]))
+
+(extend-protocol ConjColumn
+  Model
+  (conj-column! [_ _known-column-names]
+    (println "nothing to add")
+    nil)
+
+  Column
+  (conj-column! [^Column column known-column-names]
+    (println "CONJing!")
+    (swap! known-column-names conj (.getColumnName column))))
+
 (defn query->columns
   "TODO: implement!"
-  [^Statement _parsed-query]
-  ["oh no" "TODO"])
+  [^Statement parsed-query]
+  (println "query->columns")
+  (let [column-names  (atom [])
+        column-finder (proxy
+                          [TablesNamesFinder]
+                          []
+                          (visit [visitable]
+                            (println "visiting")
+                            (println {:visitable visitable
+                                      :column-names @column-names})
+                            (let [^TablesNamesFinder this this]
+                              (conj-column! visitable column-names)
+                              (proxy-super visit visitable))))]
+#_    (.init column-finder false)
+#_    (.accept parsed-query column-finder)
+
+    (.getTables column-finder parsed-query)
+    #_    @column-names)
+)
+
 
 (defn parsed-query
   "Main entry point: takes a string query and returns a `Statement` object that can be handled by the other functions."
   [^String query]
   (CCJSqlParserUtil/parse query))
+
+(-> "select foo, bar from baz;"
+    parsed-query
+    query->columns)
+
 
 (defn resolve-columns
   "TODO: Make this use metadata we know about.
@@ -46,3 +90,19 @@
         tables (query->tables parsed)
         columns (query->columns parsed)]
     (resolve-columns tables columns)))
+
+
+
+
+
+(comment
+
+
+
+  @(u/prog1 (atom [])
+     (conj-column! 1 <>)
+     (conj-column! 2.0 <>)
+     (conj-column! (Integer. 8) <>))
+
+
+  )
