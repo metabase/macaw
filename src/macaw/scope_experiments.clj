4526e2b130cb979eb92ab6378db602cf60a75dc8
Chris Truter <chris@metabase.com>
2024-08-23 13:56:36 +0200

    break exaxmples out to tests

--- src/macaw/scope_experiments.clj
+++ src/macaw/scope_experiments.clj
@@ -13,7 +13,9 @@
     (instance? Table node) [:table (.getName node)]
     :else [(type node) node]))
 
-(defn- semantic-map [sql]
+(defn semantic-map
+  "Name is a bit of a shame, for now this is a fairly low level representation of how we walk the query"
+  [sql]
   (mw/fold-query (m/parsed-query sql)
                  {:every-node (fn [acc node ctx]
                                 (let [id   (m/scope-id (first ctx))
@@ -31,68 +33,25 @@
                                                (update-in [:children parent-id] (fnil conj #{}) id))
                                            acc')))
                                       (update :sequence (fnil conj []) [id node]))))}
-                 {:scopes   {}                             ;; id -> {:path [labels], :children [nodes]}
-                  :parents  {}                             ;; what scope is this inside?
-                  :children {}                             ;; what scopes are inside?
-                  :sequence []}))                          ;; [scope-id, node]
+                 {:scopes   {}                              ;; id -> {:path [labels], :children [nodes]}
+                  :parents  {}                              ;; what scope is this inside?
+                  :children {}                              ;; what scopes are inside?
+                  :sequence []}))                           ;; [scope-id, node]
 
-(comment
- (semantic-map "select x from t, u, v left join w on w.id = v.id where t.id = u.id and u.id = v.id limit 3")
- ;{:scopes {1 {:path ["SELECT"], :children [[:column "x"]]},
- ;          2 {:path ["SELECT" "FROM"], :children [[:table "t"]]},
- ;          4 {:path ["SELECT" "JOIN" "FROM"], :children [[:table "u"]]},
- ;          5 {:path ["SELECT" "JOIN" "FROM"], :children [[:table "v"]]},
- ;          6 {:path ["SELECT" "JOIN" "FROM"], :children [[:table "w"]]},
- ;          3 {:path ["SELECT" "JOIN"], :children [[:column "id"] [:table "w"] [:column "id"] [:table "v"]]},
- ;          7 {:path ["SELECT" "WHERE"],
- ;             :children [[:column "id"]
- ;                        [:table "t"]
- ;                        [:column "id"]
- ;                        [:table "u"]
- ;                        [:column "id"]
- ;                        [:table "u"]
- ;                        [:column "id"]
- ;                        [:table "v"]]}},
- ; :parents {2 1, 4 3, 5 3, 6 3, 3 1, 7 1},
- ; :children {1 #{7 3 2}, 3 #{4 6 5}},
- ; :sequence [[1 [:column "x"]]
- ;            [2 [:table "t"]]
- ;            [4 [:table "u"]]
- ;            [5 [:table "v"]]
- ;            [6 [:table "w"]]
- ;            [3 [:column "id"]]
- ;            [3 [:table "w"]]
- ;            [3 [:column "id"]]
- ;            [3 [:table "v"]]
- ;            [7 [:column "id"]]
- ;            [7 [:table "t"]]
- ;            [7 [:column "id"]]
- ;            [7 [:table "u"]]
- ;            [7 [:column "id"]]
- ;            [7 [:table "u"]]
- ;            [7 [:column "id"]]
- ;            [7 [:table "v"]]]}
+(defn- ->descendants
+  "Given a direct mapping, get back the transitive mapping"
+  [parent->children]
+  (reduce
+   (fn [acc parent-id]
+     (let [children (parent->children parent-id)]
+       (assoc acc parent-id (into (set children) (mapcat acc) children))))
+   {}
+   ;; guarantee we process each node before its parent
+   (reverse (sort (keys parent->children)))))
 
-
- (semantic-map "select t.a,b,c,d from t")
- ;{:scopes {1 {:path ["select"], :children [[:column "a"] [:column "b"] [:column "c"] [:column "d"]]},
- ;          2 {:path ["select" "from"], :children [[:table "t"]]}},
- ; :parents {2 1},
- ; :children {1 #{2}},
- ; :sequence [[1 [:column "a"]] [1 [:column "b"]] [1 [:column "c"]] [1 [:column "d"]] [2 [:table "t"]]]}
- )
-
-(defn- get-descendants-map [parent-children-map]
-  (letfn [(get-all-descendants [parent]
-            (let [children (get parent-children-map parent [])]
-              (into #{} (concat children
-                                (mapcat #(get-all-descendants %)
-                                        children)))))]
-    (into {}
-          (for [parent (keys parent-children-map)]
-            [parent (get-all-descendants parent)]))))
-
-(defn fields->tables-in-scope [sql]
+(defn fields->tables-in-scope
+  "Build a map of each to field to all the tables that are in scope when its referenced"
+  [sql]
   (let [sm                   (semantic-map sql)
         tables               (filter (comp #{:table} first second) (:sequence sm))
         scope->tables        (reduce
@@ -100,7 +59,7 @@
                                 (update m scope-id (fnil conj #{}) table-name))
                               {}
                               tables)
-        scope->descendants   (get-descendants-map (:children sm))
+        scope->descendants   (->descendants (:children sm))
         scope->nested-tables (reduce
                               (fn [m parent-id]
                                 (assoc m parent-id
@@ -115,25 +74,11 @@
                         #{table-name}
                         (scope->nested-tables scope-id))])))))
 
-(defn- fields-to-search [f->ts]
+(defn fields-to-search
+  "Get a set of qualified columns. Where the qualification was uncertain, we enumerate all possibilities"
+  [f->ts]
   (into (sorted-set)
         (mapcat (fn [[[_ column-name] table-names]]
                   (map #(vector :table % :column column-name) table-names)))
 
         f->ts))
-
-(comment
- ;; like source-columns, but understands scope
- (fields-to-search
-  (fields->tables-in-scope "select x from t, u, v left join w on w.a = v.a where t.b = u.b and u.c = v.c limit 3"))
-  ;#{[:table "t" :column "b"]
-  ;  [:table "t" :column "x"]
-  ;  [:table "u" :column "b"]
-  ;  [:table "u" :column "c"]
-  ;  [:table "u" :column "x"]
-  ;  [:table "v" :column "a"]
-  ;  [:table "v" :column "c"]
-  ;  [:table "v" :column "x"]
-  ;  [:table "w" :column "a"]
-  ;  [:table "w" :column "x"]}
- )
