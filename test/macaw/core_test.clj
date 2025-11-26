2a05135f2236ba10ad17c4d2e07915d704f6b767
Chris Truter <chris@metabase.com>
2025-11-26 20:17:32 +0200

    Demonstrate that rename is broken when CTEs have shadowing

--- test/macaw/core_test.clj
+++ test/macaw/core_test.clj
@@ -469,6 +469,25 @@ from foo")
                              :tables  {{:schema "public" :table "orders"} "purchases"}
                              :columns {{:schema "public" :table "orders" :column "x"} "xx"}}))))
 
+(def ^:private cte-before-replace
+  "WITH z AS (SELECT * FROM a)
+   SELECT c FROM z")
+
+(def ^:private cte-after-replace
+  "WITH z AS (SELECT * FROM b)
+   SELECT c FROM z")
+
+(deftest replace-within-cte-test
+  (is (= cte-after-replace
+         (m/replace-names cte-before-replace {:tables {{:table "a"} "b"}})))
+
+  ;; TODO fix this, which is broken due to shadowing
+  #_(testing "with shadowing"
+    (let [rr #(str/replace % "z" "a")]
+        (is (= (rr cte-after-replace)
+               (m/replace-names (rr cte-before-replace) {:tables {{:table "a"} "b"}}))))))
+
+
 (deftest allow-unused-test
   (is (thrown-with-msg?
        Exception #"Unknown rename"
